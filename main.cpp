d387e7ed61a6cfc82bf5e610fd4681120a78fef0
Valentin Romanov <roomwall@yandex.ru>
2019-09-13 13:43:39 +0300

    texture
    texture output add

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
-#include <glad/glad.h>
+﻿#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb_image.h>
 
 #include <iostream>
 
@@ -15,9 +16,18 @@ const unsigned int SCR_HEIGHT = 600;
 const GLchar * vertexShaderSource = R"KEK(
 
 #version 450 core
-layout ( location = 0 ) in vec3 aPos;
+layout ( location = 0 ) in vec3 position;
+layout ( location = 1 ) in vec3 color;
+layout ( location = 2 ) in vec2 textCoord;
+
+
+out vec3 aCor;
+out vec2 tCoord;
+
 void main() {
-	gl_Position = vec4( aPos, 1.0 );
+	gl_Position = vec4( position, 1.0f );
+	aCor = color;
+	tCoord = textCoord;
 }
 
 )KEK";
@@ -27,11 +37,15 @@ const GLchar * fragmentShaderSource = R"KEK(
 
 #version 450 core
 
-uniform vec4 myColor;
+uniform vec4 scale;
+uniform sampler2D myText;
+in vec3 aCor;
+in vec2 tCoord;
 
 out vec4 FragColor;
+
 void main() {
-	FragColor = myColor;
+	FragColor = texture( myText, -tCoord ) * vec4( aCor , scale.x );
 }
 
 )KEK";
@@ -75,25 +89,64 @@ int main()
 	// vertex shader
 	shader prog( vertexShaderSource, fragmentShaderSource );
 
-	// set up vertex data (and buffer(s)) and configure vertex attributes
-	// ------------------------------------------------------------------
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f, // left  
-		 0.5f, -0.5f, 0.0f, // right 
-		 0.0f,  0.5f, 0.0f  // top   
+	// create texture
+	int width, height, nrChan;
+	unsigned char * image = stbi_load( "test.jpg", &width, &height, &nrChan, 0 );
+
+	if ( !image )
+		std::cout << "fuck you" << std::endl;
+
+	GLuint texture;
+	glGenTextures( 1, &texture );
+
+	glBindTexture( GL_TEXTURE_2D, texture );
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image );
+	glGenerateMipmap( GL_TEXTURE_2D );
+
+	stbi_image_free( image );
+	glBindTexture( GL_TEXTURE_2D, 0 );
+
+	unsigned int indices[] = {  
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+
+	GLfloat vertices[] = {
+    // Позиции          // Цвета             // Текстурные координаты
+     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // Верхний правый
+     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // Нижний правый
+    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // Нижний левый
+    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // Верхний левый
 	};
 
-	unsigned int VBO, VAO;
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers( 1, &EBO );
 	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
 	glBindVertexArray(VAO);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer( GL_ARRAY_BUFFER, VBO );
+	glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
+	glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, EBO );
+	glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( indices ), indices, GL_STATIC_DRAW );
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // texture coord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
 	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -121,9 +174,13 @@ int main()
 
 		// draw our first triangle
 		prog.Use();
-		prog.Uniform4f( "myColor", 1.0f, 0.0f, abs( sin( glfwGetTime() ) ), 1.0f );
+		prog.Uniform4f( "scale", abs( sin( glfwGetTime() ) ), 0.0f, 1.0f, 1.0f );
+		
+		glBindTexture( GL_TEXTURE_2D, texture );
 		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0 );
+		
+		glBindVertexArray( 0 );
 		// glBindVertexArray(0); // no need to unbind it every time 
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
