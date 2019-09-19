ebd84fdb9f518d19d42e2ef18c5ee8848e252a6a
Valentin Romanov <roomwall@yandex.ru>
2019-09-19 22:06:20 +0300

    Start refactoring
    Time to big change

--- main.cpp
+++ main.cpp
@@ -116,91 +116,45 @@ int main()
 	};
 
 
-
-	glm::mat4 model = glm::make_mat4( matrixLol );
-
-	glm::mat4 view = glm::make_mat4( matrixLol );
-	view = glm::translate( view, glm::vec3( 0.0f, 0.0f, -2.0f ) );
-
-	glm::mat4 projection = glm::make_mat4( matrixLol );
-	projection = glm::perspective( 45.0f, SCR_WIDTH / ( float ) SCR_HEIGHT, 0.1f, 100.0f );
-
-
 	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 	glGenBuffers( 1, &EBO );
-	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
 	glBindVertexArray(VAO);
 
 	glBindBuffer( GL_ARRAY_BUFFER, VBO );
 	glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
 
 
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)0);
-    glEnableVertexAttribArray(0);
-    // texture coord attribute
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
-    glEnableVertexAttribArray(1);
-    // texture coord attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
 
-	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
 	glBindVertexArray(0);
 
-
-	// uncomment this call to draw in wireframe polygons.
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	glEnable( GL_DEPTH_TEST );
-	// render loop
-	// -----------
+
 	while (!glfwWindowShouldClose(window))
 	{
 		// input
-		// -----
 		processInput(window);
 
 		// render
-		// ------
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 		prog.Use();
 		text.Bind();
-		// draw our first triangle
-		model = glm::make_mat4( matrixLol );
-		model = glm::rotate( model, glm::radians( ( float ) glfwGetTime() * 90.0f ), glm::vec3( 1.0f, 0.0f, 0.0f ) );
-		model = glm::rotate( model, glm::radians( ( float ) glfwGetTime() * 90.0f ), glm::vec3( 0.0f, 1.0f, 0.0f ) );
-
-		view = glm::make_mat4( matrixLol );
-		view = glm::translate( view, glm::vec3( 0.0f, 0.0f, -2.0f ) );
-
-		prog.Matrix4( "model", glm::value_ptr( model ) );
-		prog.Matrix4( "view", glm::value_ptr( view ) );
-		prog.Matrix4( "projection", glm::value_ptr( projection ) );
 
 
-			glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-			glDrawArrays( GL_TRIANGLES, 0, 36 );
-			//glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0 );
-			view = glm::make_mat4( matrixLol );
-		view = glm::translate( view, glm::vec3( 2.0f, 0.0f, -5.0f ) );
-		prog.Matrix4( "view", glm::value_ptr( view ) );
-			glDrawArrays( GL_TRIANGLES, 0, 36 );
+		// prog.Matrix4( "model", glm::value_ptr( model ) );
+		// prog.Matrix4( "view", glm::value_ptr( view ) );
+		// prog.Matrix4( "projection", glm::value_ptr( projection ) );
 
-			view = glm::make_mat4( matrixLol );
-		view = glm::translate( view, glm::vec3( -2.0f, 0.0f, -5.0f ) );
-		prog.Matrix4( "view", glm::value_ptr( view ) );
 		glDrawArrays( GL_TRIANGLES, 0, 36 );
 
-			glBindVertexArray( 0 );
-		// glBindVertexArray(0); // no need to unbind it every time
-
-		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-		// -------------------------------------------------------------------------------
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
