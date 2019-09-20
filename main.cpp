86f7bba6a25dadc9eea760aaeacbc8a14f8592de
Valentin Romanov <roomwall@yandex.ru>
2019-09-20 15:45:21 +0300

    Camera
    First Person Camera

--- main.cpp
+++ main.cpp
@@ -16,8 +16,10 @@ void processInput(GLFWwindow* window);
 GLFWwindow * AllInit();
 
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+unsigned int SCR_WIDTH = 800;
+unsigned int SCR_HEIGHT = 600;
+
+double lastX = 0, lastY = 0;
 
 const GLchar * vertexShaderSource = R"KEK(
 
@@ -54,6 +56,15 @@ void main() {
 
 )KEK";
 
+glm::vec3 cameraPos( 0.0f, 0.0f, 3.0f );
+glm::vec3 cameraFront( 0.0f, 0.0f, -1.0f );
+glm::vec3 cameraUp( 0.0f, 1.0f, 0.0f );
+GLfloat cameraSpeed = 0.05f;
+
+double pitch = 0.0, yaw = 0.0;
+bool firstMouse = true;
+
+void mouse_callback( GLFWwindow * window, double xpos, double ypos );
 
 int main()
 {
@@ -61,7 +72,11 @@ int main()
 
 	shader prog( vertexShaderSource, fragmentShaderSource );
 
-	texture text( "test.jpg" );
+	texture text( "stone.jpg" );
+
+	glfwSwapInterval( 1 );
+	glfwSetInputMode( window, GLFW_CURSOR, GLFW_CURSOR_DISABLED );
+	glfwSetCursorPosCallback( window, mouse_callback );
 
 
     GLfloat vertices[] = {
@@ -108,19 +123,18 @@ int main()
         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
 
-	GLfloat matrixLol[] = {
-		1.0f, 0.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, 0.0f, 0.0f,
-		0.0f, 0.0f, 1.0f, 0.0f,
-		0.0f, 0.0f, 0.0f, 1.0f
+	std::vector< glm::vec3 > cubes = {
+		glm::vec3( -1.0f, -1.0f, 0.0f ),
+		glm::vec3( 1.0f, 1.0f, 0.0f ),
+		glm::vec3( -1.0f, 1.0f, 0.0f ),
+		glm::vec3( 1.0f, -1.0f, 0.0f )
 	};
-
 
 	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 	glGenBuffers( 1, &EBO );
-	glBindVertexArray(VAO);
+	glBindVertexArray( VAO );
 
 	glBindBuffer( GL_ARRAY_BUFFER, VBO );
 	glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
@@ -137,53 +151,96 @@ int main()
 
 	glEnable( GL_DEPTH_TEST );
 
+
 	while (!glfwWindowShouldClose(window))
 	{
-		// input
 		processInput(window);
-
+		glBindVertexArray( VAO );
 		// render
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 		prog.Use();
 		text.Bind();
 
+		for ( int i = 0; i < cubes.size(); ++i ) {
 
-		// prog.Matrix4( "model", glm::value_ptr( model ) );
-		// prog.Matrix4( "view", glm::value_ptr( view ) );
-		// prog.Matrix4( "projection", glm::value_ptr( projection ) );
+			glm::mat4 model( 1.0f ), view( 1.0f ), projection( 1.0f );
 
-		glDrawArrays( GL_TRIANGLES, 0, 36 );
+			model = glm::translate( model, cubes[ i ] );
+			view = glm::lookAt( cameraPos, cameraPos + cameraFront, cameraUp );
+			projection = glm::perspective( 45.0f, ( float ) SCR_WIDTH / SCR_HEIGHT, 0.1f, 100.0f );
 
-		glfwSwapBuffers(window);
+			prog.Matrix4( "model", glm::value_ptr( model ) );
+			prog.Matrix4( "view", glm::value_ptr( view ) );
+			prog.Matrix4( "projection", glm::value_ptr( projection ) );
+
+			glDrawArrays( GL_TRIANGLES, 0, 36 );
+		}
+		glfwSwapBuffers( window );
 		glfwPollEvents();
+		std::cout << pitch << " " << yaw << std::endl;
 	}
 
-	// optional: de-allocate all resources once they've outlived their purpose:
-	// ------------------------------------------------------------------------
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
 
-	// glfw: terminate, clearing all previously allocated GLFW resources.
-	// ------------------------------------------------------------------
 	glfwTerminate();
 	return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+	if ( glfwGetKey( window, GLFW_KEY_W ) == GLFW_PRESS )
+		cameraPos += cameraSpeed * cameraFront;
+	if ( glfwGetKey( window, GLFW_KEY_S ) == GLFW_PRESS )
+		cameraPos -= cameraSpeed * cameraFront;
+	if ( glfwGetKey( window, GLFW_KEY_A ) == GLFW_PRESS )
+		cameraPos -= cameraSpeed * glm::normalize( glm::cross( cameraFront, cameraUp ) );
+	if ( glfwGetKey( window, GLFW_KEY_D ) == GLFW_PRESS )
+		cameraPos += cameraSpeed * glm::normalize( glm::cross( cameraFront, cameraUp ) );
+	if ( glfwGetKey( window, GLFW_KEY_Q ) == GLFW_PRESS )
+		cameraPos -= cameraSpeed * cameraUp;
+	if ( glfwGetKey( window, GLFW_KEY_E ) == GLFW_PRESS )
+		cameraPos += cameraSpeed * cameraUp;
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
+void mouse_callback( GLFWwindow * window, double xpos, double ypos ) {
+	if ( firstMouse ) {
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	GLfloat xoffset = xpos - lastX;
+	GLfloat yoffset = lastY - ypos;
+	lastX = xpos;
+	lastY = ypos;
+
+	GLfloat sens = 0.05f;
+	xoffset *= sens;
+	yoffset *= sens;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	if ( pitch > 89.0f )
+		pitch = 89.0f;
+	if ( pitch < -89.0f )
+		pitch = -89.0f;
+
+	glm::vec3 front;
+	front.x = cos( glm::radians( pitch ) ) * cos( glm::radians( yaw ) );
+	front.y = sin( glm::radians( pitch ) );
+	front.z = cos( glm::radians( pitch ) ) * sin( glm::radians( yaw ) );
+	cameraFront = glm::normalize( front ) ;
+}
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-	// make sure the viewport matches the new window dimensions; note that width and 
-	// height will be significantly larger than specified on retina displays.
+	SCR_HEIGHT = height;
+	SCR_WIDTH = width;
 	glViewport(0, 0, width, height);
 }
 
